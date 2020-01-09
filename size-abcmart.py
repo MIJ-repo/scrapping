d851a9bedfac7be879130a2e0f5906dc7e4180a1
Long <l.luu@dai.co.jp>
2020-01-09 19:13:47 +0900

    abc-size

--- size-abcmart.py
+++ size-abcmart.py
@@ -14,26 +14,17 @@ for product in products:
     soup_base = BeautifulSoup(base_page_html.text, 'html.parser')
     size_div = soup_base.find('div', {'class': 'choosed_size_list'})
     if size_div != None:
-    #   pass
-        # exit()
         sizes = size_div.select('dl')
-        print(sizes[8])
-        exit()
         for i in range(len(sizes)):
-            print(i)
             if sizes[i].find('div', {'class': 'add-cart-popup'}) != None:
-                # print(sizes[i])
                 size_filter = sizes[i].select_one('dt').getText()
                 size = size_filter.split('/')
-                print(size)
-                # exit()
                 sizes[i] = size[0].replace(' ', '').replace('cm', '')
             else:
-                del sizes[i]
-        sizes = ','.join(sizes)
-        print(sizes)
-        exit()
+                sizes[i] = None
+        available_size = list(filter(None, sizes))
+        available_size = ','.join(available_size)
 
-        database.updateSize(sizes, product['id'])
+        database.updateSize(available_size, product['id'])
     else:
         database.updateStatus(product['id'])
