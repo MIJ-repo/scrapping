a5dbdafd5a13063c8068db383e7d712a37ea22d0
Long <l.luu@dai.co.jp>
2020-02-27 20:12:15 +0900

    get product

--- cookpad-product.py
+++ cookpad-product.py
@@ -1,8 +1,43 @@
 import requests
+import re
 import json
 from datetime import datetime
 from bs4 import BeautifulSoup
-from database import Database
+from cookpad_db import Database
 database = Database()
 
-base_url = "https://www.abc-mart.net"
+def getProductUrl(url, page_no):
+    page_html = requests.get(url + '?page=' + str(page_no))
+    soup = BeautifulSoup(page_html.text, 'html.parser')
+    return soup.find_all('a', {'class': 'recipe-title'})
+
+def getProductContent(url):
+    page_html = requests.get(url)
+    soup = BeautifulSoup(page_html.text, 'html.parser')
+    print(soup)
+
+def getTotalPageNumber(url):
+    page_html = requests.get(url)
+    soup = BeautifulSoup(page_html.text, 'html.parser')
+    return int(soup.select_one('.page_num').getText().replace('1 / ', '').replace('ページ', ''))
+
+base_url = 'https://cookpad.com'
+parents_category = database.getParentsCategory()
+
+for parents in parents_category:
+    children_category = database.getChildrenCategoryByParentId(parents.get('id'))
+    for children in children_category:
+        grandchildren_category = database.getChildrenCategoryByParentId(children.get('id'))
+        for grandchildren in grandchildren_category:
+            great_grandchildrens = database.getChildrenCategoryByParentId(grandchildren.get('id'))
+            for great_grandchildren in great_grandchildrens:
+                for page_no in range(1, getTotalPageNumber(great_grandchildren.get('origin_url'))):
+                    products_url = getProductUrl(great_grandchildren.get('origin_url'), page_no)
+                    for product_url in products_url:
+                        if base_url not in product_url.attrs['href']:
+                            product_url = base_url + product_url.attrs['href']
+                        else:
+                            product_url = product_url.attrs['href']
+                        data = (great_grandchildren.get('id'), product_url, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                        database.insertProductOriginUrl(data)
+                        
--- cookpad_db.py
+++ cookpad_db.py
@@ -26,6 +26,18 @@ class Database:
         result = self.db.fetchall()
         return result[0]
 
+    def getParentsCategory(self):
+        sql = "SELECT `id` FROM `category` where `parent_flag` = 1"
+        self.db.execute(sql)
+        result = self.db.fetchall()
+        return result
+
+    def getChildrenCategoryByParentId(self, parents_id):
+        sql = "SELECT `id`, `origin_url` from `category` where `group_id` = %s"
+        self.db.execute(sql, (parents_id,))
+        result = self.db.fetchall()
+        return result
+
     def insertCategory(self, data):
         
         sql = "INSERT INTO `category` (`parent_flag`, `group_id`, `name_en`, `name_jp`, `origin_url`, `image`, `meta_title`, `meta_description`, `meta_keywords`, `active_status`, `created`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -33,6 +45,17 @@ class Database:
             self.db.execute(sql, data)
             self.connect.commit()
             print('Category saved')
+        except mysql.connector.Error as error :
+            self.connect.rollback()
+            print(error)
+            print('-------------------------------------------------')
+
+    def insertProductOriginUrl(self, data):
+        sql = "INSERT INTO `product` (`category_id`, `origin_url`, `created`) VALUES (%s, %s, %s)"
+        try:
+            self.db.execute(sql, data)
+            self.connect.commit()
+            print('Product URL saved')
         except mysql.connector.Error as error :
             self.connect.rollback()
             print(error)
