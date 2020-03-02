7656e4b21b42c706fb3612b7f554f736cb4c65bc
MIJ-repo <mij.repo@gmail.com>
2020-03-03 07:52:15 +0900

    174259

--- cookpad_db.py
+++ cookpad_db.py
@@ -68,7 +68,7 @@ class Database:
             print('-------------------------------------------------')
 
     def selectProduct(self):
-        sql = "SELECT `id`, `origin_url` FROM `product` where `id` > 155628"
+        sql = "SELECT `id`, `origin_url` FROM `product` where `id` > 174259"
         self.db.execute(sql)
         result = self.db.fetchall()
         return result
